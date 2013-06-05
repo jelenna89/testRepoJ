40d4d4c658b09eb8eeac2700702a5e00e54bd2e2
mratkovic <mratkovic@IB-MRATKOVIC.infobip.local>
2013-06-05 16:47:23 +0200

    Promenjen drugi string dodat metod

--- TestRepo/TestRepo/src/test/Test.java
+++ TestRepo/TestRepo/src/test/Test.java
@@ -3,7 +3,11 @@ package test;
 public class Test {
 
 	String string="Jelena";
-	String string3="Jelena2";
+	String string3="Jel";
 	
+	
+	public static void main(String [] args) {
+		System.out.println("Hello");
+	}
 	
 }
