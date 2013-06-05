dc27789d77f7feb15a5205d7d22a8bcefcd53b9a
mratkovic <mratkovic@IB-MRATKOVIC.infobip.local>
2013-06-05 17:00:46 +0200

    Nova f-ja i promenljiva

--- TestRepo/TestRepo/src/test/Test.java
+++ TestRepo/TestRepo/src/test/Test.java
@@ -3,11 +3,20 @@ package test;
 public class Test {
 
 	String string="Jelena";
+	private int num = 13;
 	String string3="JelenaStoj..";
 	
 	
 
 	String konflikt="KOnflikt";
 
+	
+	private boolean isOdd() {
+		if (num / 2 == 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 	
 }
